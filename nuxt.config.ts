0c4b3f8509cfe35b0fb85eb0811fa968f8500c71
Janwyyy <adminjanry@ASUS-TUF-GAMING-A15>
2026-04-20 19:11:57 +0800

    TypeScript, TailwindCSS, Google Fonts Setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,17 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ["~/main.css"],
+  modules: ['@nuxt/ui', '@nuxtjs/google-fonts'],
+
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+          //href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700&display=swap' IF YOU WANT TO ADD ANOTHER FONT
+        }
+      ]
+    }
+  }
 })
